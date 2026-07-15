2dbc2a1d74d15480cc75093fd324a17ca6305fc1
unknown <llioueboyp@gmail.com>
2026-07-15 17:43:47 +0800

    update

--- tests/assign_permission/test_assign_permission_delete.py
+++ tests/assign_permission/test_assign_permission_delete.py
@@ -3,14 +3,8 @@ import allure
 
 
 @allure.title("[ASSIGN-PERMISSION-CRUD] Delete assign permission successfully")
-def test_assign_permission_delete_success(
-    assign_permission_app: OmniApp,
-    created_assign_permission,
-):
-    assign_permission_app.assign_permission_page.open_assign_permission_delete_dialog(
-        created_assign_permission.role_code
-    )
+def test_assign_permission_delete_success(assign_permission_app: OmniApp, created_assign_permission):
+    assign_permission_app.assign_permission_page.open_assign_permission_delete_dialog(created_assign_permission.role_code)
     assign_permission_app.assign_permission_page.verify_deleted_input()
-    assign_permission_app.assign_permission_page.verify_assign_permission_deleted(
-        created_assign_permission.role_code
-    )
+    assign_permission_app.assign_permission_page.verify_assign_permission_deleted(created_assign_permission.role_code)
+    
--- tests/assign_permission/test_assign_permission_read.py
+++ tests/assign_permission/test_assign_permission_read.py
@@ -3,13 +3,8 @@ import allure
 
 
 @allure.title("[ASSIGN-PERMISSION-CRUD] Read assign permission successfully")
-def test_assign_permission_read_success(
-    assign_permission_app: OmniApp,
-    created_assign_permission,
-):
+def test_assign_permission_read_success(assign_permission_app: OmniApp, created_assign_permission):
     assign_permission_app.assign_permission_page.verify_assign_permission_list_visible()
-    assign_permission_app.assign_permission_page.search_assign_permission_by_member(
-        created_assign_permission.second_member
-    )
+    assign_permission_app.assign_permission_page.search_assign_permission_by_member(created_assign_permission.second_member)
     assign_permission_app.assign_permission_page.search_assign_permission_with_no_result()
     assign_permission_app.assign_permission_page.sort_assign_permissions_by_created_time()
